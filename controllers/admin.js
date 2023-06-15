7d3be6cd00a6b951f642dd8907e69e12f26784a0
Mohannadgithub <mohannadattal85@gmail.com>
2023-06-15 15:10:08 +0200

    Worked with delete product

--- controllers/admin.js
+++ controllers/admin.js
@@ -89,17 +89,14 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-// exports.postDeleteProduct = (req, res, next) => {
-//   const prodId = req.body.productId;
-//   Product.findByPk(prodId)
-//     .then((product) => {
-//       return product.destroy();
-//     })
-//     .then((result) => {
-//       console.log('DELETED PRODUCT');
-//       res.redirect('/admin/products');
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// };
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.deleteById(prodId)
+    .then(() => {
+      console.log('DELETED PRODUCT');
+      res.redirect('/admin/products');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
