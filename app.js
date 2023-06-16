a9d5e9d4957aa5458f046374bfbb383a2d812957
Mohannadgithub <mohannadattal85@gmail.com>
2023-06-16 11:54:47 +0200

    Worked to Add Cart Functionality, Store Multiple Products in the Cart, Display the cart items

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
   User.findById('648b1a1275b27910aed692a0')
-  .then((user) => {
-      req.user = user;
+    .then((user) => {
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch((err) => console.log(err));
